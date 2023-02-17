d9152a2ed98c54854039e83ffc4caef949eecfee
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2023-02-17 10:01:24 +0530

    Create basics_r.r

---
+++ basics_r.r
@@ -0,0 +1,26 @@
+i="hello"
+> print(i)
+[1] "hello"
+> print(typeof(i))
+[1] "character"
+> d=34
+> f=2
+> print(d==f)
+[1] FALSE
+> print(d+f)
+[1] 36
+> print("jai ho")
+[1] "jai ho"
+> name <- yoyo
+Error: object 'yoyo' not found
+> name <- "yoyo banta rapper"
+> name
+[1] "yoyo banta rapper"
+> print(d<2)
+[1] FALSE
+> print(d>f)
+[1] TRUE
+> d+f
+[1] 36
+> d-f
+[1] 32
