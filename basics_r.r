83d2d8c311f8683ebe9a6ff9e911dc81e2cfd50b
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2023-02-27 11:43:32 +0530

    Update basics_r.r

--- basics_r.r
+++ basics_r.r
@@ -24,3 +24,11 @@ Error: object 'yoyo' not found
 [1] 36
 > d-f
 [1] 32
+ #getwd is used to check the current working directory
+> # setwd(complete_path_of_new_folder)to change the directory
+> # read/import file - 1. "data()" if in built in repository used
+# read/import file - 1. "read.extension(file_name)" if personal/user file in the same working directory used
+> # read/import file - 1. "read.extension(complete_path)" if personal/user file in different working directory used
+> #while read/import, make sure the required package is installed according to what type of file is being imported
+# package "dplyr" is used for data manipulation
+> # to use the package, to uncover the package, "library(package_name)" is used
